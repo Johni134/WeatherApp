82bdd961e18c817d3076262d6a773af83fffc2a4
Johni134 <kec@inbox.ru>
2018-04-10 22:50:10 +0700

    fixed crash on exit (WeatherModel now implements Parcelable)

--- app/src/main/java/ru/geekbrains/evgeniy/weatherapp/model/WeatherModel.java
+++ app/src/main/java/ru/geekbrains/evgeniy/weatherapp/model/WeatherModel.java
@@ -1,8 +1,43 @@
 package ru.geekbrains.evgeniy.weatherapp.model;
 
-public class WeatherModel {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class WeatherModel implements Parcelable {
     public long id;
     public String main;
     public String description;
     public String icon;
+
+    protected WeatherModel(Parcel in) {
+        id = in.readLong();
+        main = in.readString();
+        description = in.readString();
+        icon = in.readString();
+    }
+
+    public static final Creator<WeatherModel> CREATOR = new Creator<WeatherModel>() {
+        @Override
+        public WeatherModel createFromParcel(Parcel in) {
+            return new WeatherModel(in);
+        }
+
+        @Override
+        public WeatherModel[] newArray(int size) {
+            return new WeatherModel[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(id);
+        dest.writeString(main);
+        dest.writeString(description);
+        dest.writeString(icon);
+    }
 }
