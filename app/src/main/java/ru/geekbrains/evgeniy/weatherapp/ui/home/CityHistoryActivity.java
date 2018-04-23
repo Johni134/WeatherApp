5a7be040f4bbf50f5efaa0ed7bd307c6e0ae96af
Johni134 <kec@inbox.ru>
2018-04-24 00:07:48 +0700

    +history for city

--- app/src/main/java/ru/geekbrains/evgeniy/weatherapp/ui/home/CityHistoryActivity.java
+++ app/src/main/java/ru/geekbrains/evgeniy/weatherapp/ui/home/CityHistoryActivity.java
@@ -3,14 +3,20 @@ package ru.geekbrains.evgeniy.weatherapp.ui.home;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import ru.geekbrains.evgeniy.weatherapp.R;
 import ru.geekbrains.evgeniy.weatherapp.data.WeatherDataLoader;
+import ru.geekbrains.evgeniy.weatherapp.model.HistoryModel;
 import ru.geekbrains.evgeniy.weatherapp.model.HistoryModelArray;
 import ru.geekbrains.evgeniy.weatherapp.ui.home.adapters.HistoryAdapter;
 
@@ -25,8 +31,11 @@ public class CityHistoryActivity extends AppCompatActivity {
 
     public static final String EXTRA_CITY_ID = "EXTRA_CITY_ID";
     public static final String EXTRA_CITY_TITLE = "EXTRA_CITY_TITLE";
+    private static final String CONST_PARCELABLE_LIST = "const_parcelable_list";
 
     private HistoryAdapter historyAdapter;
+
+    private List<HistoryModel> historyModelList;
 
     Handler handler = new Handler();
 
@@ -47,18 +56,33 @@ public class CityHistoryActivity extends AppCompatActivity {
         textViewTitle.setText(cityTitle);
         textViewHistory.setText(getString(R.string.history_for_5_days));
 
-        new Thread() {
-            public void run() {
-                final HistoryModelArray historyModelArray = WeatherDataLoader.getHistoryWeatherByID(cityId.toString());
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        historyAdapter = new HistoryAdapter(historyModelArray.list);
-                        recyclerViewHistory.setAdapter(historyAdapter);
-                    }
-                });
+        if (savedInstanceState != null)
+        {
+            historyModelList = savedInstanceState.getParcelableArrayList(CONST_PARCELABLE_LIST);
+            if(historyModelList != null) {
+                historyAdapter = new HistoryAdapter(historyModelList);
+                recyclerViewHistory.setAdapter(historyAdapter);
             }
-        }.start();
+        }
+        else {
+            new Thread() {
+                public void run() {
+                    final HistoryModelArray historyModelArray = WeatherDataLoader.getHistoryWeatherByID(cityId.toString());
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (historyModelArray == null || historyModelArray.list.size() == 0) {
+                                Toast.makeText(getApplicationContext(), getString(R.string.place_not_found), Toast.LENGTH_SHORT).show();
+                            } else {
+                                historyModelList = historyModelArray.list;
+                                historyAdapter = new HistoryAdapter(historyModelList);
+                                recyclerViewHistory.setAdapter(historyAdapter);
+                            }
+                        }
+                    });
+                }
+            }.start();
+        }
     }
 
     private void initViews() {
@@ -71,5 +95,9 @@ public class CityHistoryActivity extends AppCompatActivity {
         recyclerViewHistory.setHasFixedSize(true);
     }
 
-
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putParcelableArrayList(CONST_PARCELABLE_LIST, (ArrayList<? extends Parcelable>) historyModelList);
+        super.onSaveInstanceState(outState);
+    }
 }
